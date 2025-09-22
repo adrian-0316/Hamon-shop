176fea1bd5ad8f515c23212bff1e6ec73f53f1f0
adrian <adrianzharcov@gmail.com>
2025-09-22 22:19:58 +0300

    project created

---
+++ src/main/java/com/example/jamonshop/JamonshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jamonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamonshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamonshopApplication.class, args);
+	}
+
+}
