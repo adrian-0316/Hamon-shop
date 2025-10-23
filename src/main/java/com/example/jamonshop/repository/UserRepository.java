8e3b66a0d39fda9430ee3e3d78c341d89cb183d0
adrian <adrianzharcov@gmail.com>
2025-10-23 23:56:00 +0300

    User entity added

---
+++ src/main/java/com/example/jamonshop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.jamonshop.repository;
+
+import com.example.jamonshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
